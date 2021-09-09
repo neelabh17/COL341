ec9aecb90a0b74c741aada95c88b726fdb4e6365
neelabh17 <neelabh.madan@outlook.com>
2021-09-09 18:36:40 +0530

    1.1.c finding lambda

--- Assignment_1/feature_selection.py
+++ Assignment_1/feature_selection.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
+from tqdm import tqdm
 
-
+def transforms(X):
+    pass
 
 # import the data
 def main(args):
@@ -27,59 +29,61 @@ def main(args):
     test=data.drop(train_val.index)
     train = train_val.sample(frac=0.8,random_state=200)
     val = train_val.drop(train.index)
+    train_val_y = train_val["Total Costs"].to_numpy()
+    train_val.drop("Total Costs", inplace = True, axis = 1)
+    train_val_x = train_val
+
     # data_Y = data["Total Costs"]
     # data_X = data
     print(train.shape, val.shape, test.shape)
 
 
     # Super basic baseline
-    Y = train_val["Total Costs"].to_numpy()
-    train_val.drop("Total Costs", inplace = True, axis = 1)
-    X = train_val
 
 
+    lambdas = np.genfromtxt("regularization.txt", delimiter = ",")
+    print(lambdas)
     # Lasso LARS
-    reg = linear_model.LassoLars(alpha=0.01)
-    reg.fit(X, Y)
+    for lam in tqdm(lambdas):
+        Y = train_val_y
+        X = train_val_x
+        print(X,Y.shape)
+        reg = linear_model.LassoLars(alpha=lam)
+        # reg.fit(X, Y)
 
 
-    # normal
-    # W = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, Y))
+        # normal
+        # W = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, Y))
 
-    # polynomial
-    trans = PolynomialFeatures(degree=2)
-    X_new = trans.fit_transform(X)
-    X = X_new
-    reg.fit(X_new, Y)
-    X = X_new.T[reg.coef_ != 0].T
-    
-    import pdb; pdb.set_trace()
-
-
-    # dummy = [1 for _ in range(train_val.shape[0])]
-    # X["dummy"] = dummy
-    # X = X.to_numpy()
-
-    W = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, Y))
+        # polynomial
+        trans = PolynomialFeatures(degree=2)
+        X_new = trans.fit_transform(X)
+        X = X_new
+        reg.fit(X_new, Y)
+        X = X_new.T[reg.coef_ != 0].T
+        # import pdb; pdb.set_trace()
 
 
-    import pdb; pdb.set_trace()
+        dummy = np.array([1 for _ in range(X.shape[0])]).reshape(-1,1)
+        X = np.concatenate((X,dummy), axis = 1)
+        W = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, Y))
 
 
-    Y = test["Total Costs"].to_numpy()
-    test.drop("Total Costs", inplace = True, axis = 1)
-    X = test
+        # import pdb; pdb.set_trace()
 
-    dummy = [1 for _ in range(test.shape[0])]
-    X["dummy"] = dummy
-    X = X.to_numpy()
 
-    trans = PolynomialFeatures(degree=2)
-    X_new = trans.fit_transform(X)
-    X = X_new
+        Y = test["Total Costs"].to_numpy()
+        test.drop("Total Costs", inplace = True, axis = 1)
+        X = test.to_numpy()
+        X_new = trans.fit_transform(X)
+        X = X_new.T[reg.coef_ != 0].T
+        dummy = np.array([1 for _ in range(X.shape[0])]).reshape(-1,1)
+        X = np.concatenate((X,dummy), axis = 1)
+        # import pdb; pdb.set_trace()
 
-    print(np.linalg.norm(np.matmul(X, W)- Y)/ np.linalg.norm((Y)))
+        print(lam, np.linalg.norm(np.matmul(X, W)- Y)/ np.linalg.norm(Y), X.shape[1])
 
+    # import pdb; pdb.set_trace()
 
     
 
