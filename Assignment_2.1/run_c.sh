7266f5b56d31d696549c8eaed8a6d4ab6f720cc9
Jatin Prakash <jatinprakash1511@gmail.com>
2021-10-11 20:15:31 +0530

    submission v0.1

---
+++ Assignment_2.1/run_c.sh
@@ -0,0 +1,2 @@
+python neural_c.py data/ outputs/ essentials/part_c/param.txt.txt
+# python neural_c1.py data/ essentials/part_c essentials/part_c/param.txt.txt
---
+++ Assignment_2.1/run_d.sh
@@ -0,0 +1,2 @@
+python neural_d.py data/ outputs/
+# python neural_c1.py data/ essentials/part_c essentials/part_c/param.txt.txt
